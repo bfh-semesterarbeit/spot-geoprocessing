4646ddcea5548e081a6935b87417d0f0d78581db
Tobias Reber <toby@seidenfein.ch>
2020-08-21 11:35:32 +0200

    Ausarbeitung Prototyp

--- latex/semesterarbeit/src/help_commands.sh
+++ latex/semesterarbeit/src/help_commands.sh
@@ -21,7 +21,19 @@ node --max-old-space-size=20000 node_modules/.bin/roadhouse -i input -o output -
 
 node --max-old-space-size=20000 node_modules/.bin/roadhouse -i /var/app/input -o /var/app/output --db /var/app/db --clear-normals --face-normals --max-tiles 1000 -r UUID -r DATUM_AENDERUNG -r DATUM_ERSTELLUNG -r ERSTELLUNG_JAHR -r ERSTELLUNG_MONAT -r REVISION_JAHR -r REVISION_MONAT -r GRUND_AENDERUNG -r HERKUNFT -r HERKUNFT_JAHR -r HERKUNFT_MONAT -r OBJEKTART -r ORIGINAL_HERKUNFT -r GEBAEUDE_NUTZUNG -r Longitude -r Latitude -r Height
 
-docker run -it -v /home/ubuntu/data/input/:/var/app/input/ -v /home/ubuntu/data/output/:/var/app/output/ -v /home/ubuntu/data/db:/var/app/db -v /home/ubuntu/data/agi_log/:/var/app/log c6105ce43be3
+docker run -it   -m 55G --cpus=15 -v /home/ubuntu/data/input/:/var/app/input/ -v /home/ubuntu/data/output/:/var/app/output/ -v /home/ubuntu/data/db:/var/app/db -v /home/ubuntu/data/log/:/var/app/log c6105ce43be3
+
+
+node --max-old-space-size=20000 node_modules/.bin/roadhouse -i /var/app/input -o /var/app/output --db /var/app/db/processing --clear-normals --face-normals --max-tiles 1000 -r UUID -r DATUM_AENDERUNG -r DATUM_ERSTELLUNG -r ERSTELLUNG_JAHR -r ERSTELLUNG_MONAT -r REVISION_JAHR -r REVISION_MONAT -r GRUND_AENDERUNG -r HERKUNFT -r HERKUNFT_JAHR -r HERKUNFT_MONAT -r OBJEKTART -r ORIGINAL_HERKUNFT -r GEBAEUDE_NUTZUNG -r Longitude -r Latitude -r Height 2>&1 > /var/app/log/agi.log &
+
 
 # yeah:
 docker run -it  -m 55G --cpus=15 -v /home/ubuntu/data/input/:/var/app/input/ -v /home/ubuntu/data/output/:/var/app/output/ -v /home/ubuntu/db:/var/app/db c6105ce43be3 /root/.nvm/versions/node/v8.11.2/bin/node --max-old-space-size=20000 /var/app/node_modules/.bin/roadhouse -i /var/app/input -o /var/app/output -db /var/app/db --clear-normals --face-normals --max-tiles 1000 -r UUID -r DATUM_AENDERUNG -r DATUM_ERSTELLUNG -r ERSTELLUNG_JAHR -r ERSTELLUNG_MONAT -r REVISION_JAHR -r REVISION_MONAT -r GRUND_AENDERUNG -r HERKUNFT -r HERKUNFT_JAHR -r HERKUNFT_MONAT -r OBJEKTART -r ORIGINAL_HERKUNFT -r GEBAEUDE_NUTZUNG -r Longitude -r Latitude -r Height
+
+
+
+for i in /home/ubuntu/data_ssd/buildings/*.tar; do echo extracting ${i};tar -xzvf ${i} -C /home/ubuntu/data_ssd/input/;done
+
+
+
+/var/app/input/1272-33/1272-33.kml\n---------------------------------\nErrors:\n\tModel file "models/model_1056.dae"
